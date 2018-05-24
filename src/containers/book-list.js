671499a22f9a68a853f22c1b712fed3c0780025c
Simba <nysochp@gmail.com>
2018-05-25 02:15:30 +0800

    little change - complete

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -8,7 +8,7 @@ class BookList extends Component {
         return this.props.books.map((book) => {
             return (
                 <li 
-                    key="{book.title}" 
+                    key={book.title}
                     className="list-group-item"
                     onClick={() => this.props.selectBook(book)}
                     >
